a199d9793cec2a300de172d708828fefc2459c8a
Notgamer07 <tyjagrath@gmail.com>
2025-03-18 13:14:12 +0530

    fixed the RR rotation

--- AVLtreeP.cpp
+++ AVLtreeP.cpp
@@ -34,11 +34,40 @@ class AVLtree{
         cout<<node->data<<"->";
         inorder(node->right);
     }
-    void insert()
+    int height(Node* node){
+        if(node == nullptr) return 0;
+        int left = height(node->left);
+        int right = height(node->right);
+        return max(left,right)+1;
+    }
+    int heightFactor(Node* node){
+        return height(node->left)-height(node->right);
+    }
+    Node* insert(){
+        int val;
+        cout<<"Enter data :";
+        cin>>val;
+        if(val == -1) return nullptr;
+        Node* node= new Node(val);
+        cout<<"Enter the right of "<<val<<" :";
+        node->right = insert();
+        int heightfactor = heightFactor(node);
+        if(heightfactor < -1){ //RR rotation
+            Node* x = node->right;
+            Node* y = x->left;
+            x->left = node;
+            node->right = y;
+            return x; 
+        }
+        else return node;
+    }
 };
 int main(){
     AVLtree tree;
-    tree.head=tree.enter();
-    tree.inorder(tree.head);
+    tree.head=tree.insert();
+    cout<<endl<<"root ->"<<tree.head->data;
+    AVLtree t;
+    t.head=t.enter();
+    cout<<endl<<"head->"<<t.head->data;
     return 0;
 }
