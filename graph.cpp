100fcf5a671e83ad5890d92389c98bb8f0280936
Notgamer07 <tyjagrath@gmail.com>
2025-06-07 18:01:47 +0530

    graph representation

---
+++ graph.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int node, routes;
+    cin>>node>>routes;
+    int graph[node+1][node+1];
+    for (int i=0;i<routes;i++){
+        int u,v;
+        cin>>u>>v;
+        graph[u][v] = 1;
+        graph[v][u] = 1;
+    }
+    for(int i=0;i<routes;i++){
+        for (int j=0;j<routes;j++){
+            if (graph[i][j] == 1){
+                cout<<i<<"<->"<<j<<endl;
+            }
+        }
+    }
+}
